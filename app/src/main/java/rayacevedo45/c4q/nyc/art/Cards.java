4bf7df88984dd5ba1be290401a726503e4c23414
abassawo <abass.bayo@gmail.com>
2015-06-23 02:55:33 -0400

    Created scrollable skeleton card-structure

--- app/src/main/java/rayacevedo45/c4q/nyc/art/Cards.java
+++ app/src/main/java/rayacevedo45/c4q/nyc/art/Cards.java
@@ -23,7 +23,7 @@ public class Cards extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
 
-        ExpandableListView listView = (ExpandableListView) findViewById(R.id.listview);
+        //ExpandableListView listView = (ExpandableListView) findViewById(R.id.listview);
 
         Bundle extras = getIntent().getExtras();
         name = extras.getString("name");
@@ -33,36 +33,7 @@ public class Cards extends ActionBarActivity {
         welcome = (TextView) findViewById(R.id.welcomeTV);
         welcome.setText("Hello, " + name);
 
-        ArrayList<Group> groups = prepareData();
-        final CustomExpandableListAdapter adapter = new CustomExpandableListAdapter(this, groups);
-        ListView listview = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(adapter);
-
-        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                Group group = (Group) adapter.getGroup(groupPosition);
-
-                Toast
-                        .makeText(getApplicationContext(), group.mName, Toast.LENGTH_SHORT)
-                        .show();
-
-                return false;
-            }
-        });
-
-        listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                String child = (String) adapter.getChild(groupPosition, childPosition);
-
-                Toast
-                        .makeText(getApplicationContext(), child, Toast.LENGTH_SHORT)
-                        .show();
-
-                return false;
-            }
-        });
+        //ArrayList<Group> groups = prepareData();
 
     }
 
@@ -89,32 +60,32 @@ public class Cards extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public ArrayList<Group> prepareData() {
-
-        Group group1 = new Group("Weather");
-        group1.children.add("SAMPLE");
-        group1.children.add("test");
-
-        Group group2 = new Group("Julio Boes");
-        group2.children.add("wr=sss");
-        group2.children.add("xxxxxxx");
-
-        Group group3 = new Group("Ron Osmun");
-        group3.children.add("ron.osmun@gmail.com");
-        group3.children.add("osmun.ron@gmail.com");
-
-        Group group4 = new Group("Angelica Tebbs");
-        group4.children.add("angelica.tebbs@gmail.com");
-        group4.children.add("tebbs.angelica@gmail.com");
-
-        ArrayList<Group> groups = new ArrayList<Group>();
-        groups.add(group1);
-        groups.add(group2);
-        groups.add(group3);
-        groups.add(group4);
-
-        return groups;
-    }
+//    public ArrayList<Group> prepareData() {
+//
+//        Group group1 = new Group("Weather");
+//        group1.children.add("SAMPLE");
+//        group1.children.add("test");
+//
+//        Group group2 = new Group("Julio Boes");
+//        group2.children.add("wr=sss");
+//        group2.children.add("xxxxxxx");
+//
+//        Group group3 = new Group("Ron Osmun");
+//        group3.children.add("ron.osmun@gmail.com");
+//        group3.children.add("osmun.ron@gmail.com");
+//
+//        Group group4 = new Group("Angelica Tebbs");
+//        group4.children.add("angelica.tebbs@gmail.com");
+//        group4.children.add("tebbs.angelica@gmail.com");
+//
+//        ArrayList<Group> groups = new ArrayList<Group>();
+//        groups.add(group1);
+//        groups.add(group2);
+//        groups.add(group3);
+//        groups.add(group4);
+//
+//        return groups;
+//    }
 
 
 
